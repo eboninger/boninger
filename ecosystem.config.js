33a79ee408e8c7ee31437fba7da9dbaa5ea5384c
Eli Boninger <eli.boninger@slalom.com>
2017-11-20 21:17:05 -0500

    setting up EAN stack

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    apps: [
+        {
+            name: 'boninger',
+            script: './server.js'
+        }
+    ],
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            host: 'ec2-34-237-114-155.compute-1.amazonaws.com',
+            key: '~/.ssh/node-server-key-pair.pem',
+            ref: 'origin/master',
+            repo: 'https://github.com/eboninger/node-server.git',
+            path: '/home/ubuntu/boninger',
+            'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+        }
+    }
+};
